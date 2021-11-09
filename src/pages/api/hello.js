393174ad0ce6c4d8ba24e73f912b2a85ec6950d2
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-11-08 23:30:31 -0300

    making some http requests

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res
+    .status(200)
+    .json([
+      [
+        { name: "John Doe" },
+        { name: "John" },
+        { name: "Doe" },
+        { name: "Jasohn Doee" },
+      ],
+    ]);
 }
